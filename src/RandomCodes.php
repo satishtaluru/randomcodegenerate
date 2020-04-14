e7ebbd397cb8cf4ca256f837985e7287c5fbc636
satish taluru <satish.taluru@firsthive.com>
2020-04-14 12:11:04 +0530

    Added new generate code and testing

--- src/RandomCodes.php
+++ src/RandomCodes.php
@@ -2,6 +2,44 @@
 namespace RandomCodes;
 class RandomCodes{
     public function generateCode(){
-        return rand(10000,99999);
+        return $randNo = mt_rand(1000, 9999) . mt_rand(10000000, 99999999);
     }
+
+    public function generateAlphabets($length = 8, $isCapital = false) {
+
+        $listAlpha = 'abcdefghijklmnopqrstuvwxyz';        
+        $shortCode = str_shuffle(
+                substr(str_shuffle($listAlpha), 0, $length)
+        );
+        if($isCapital){
+            return strtoupper($shortCode);
+        }
+        return ($shortCode);
+    }
+
+    public function generateNumericCode($length = 8, $incZero = false) {
+    
+        if ($incZero) {
+            $listAlpha = '0123456789';
+        } else {
+            $listAlpha = '123456789';
+        }
+        $shortCode = str_shuffle(
+                substr(str_shuffle($listAlpha), 0, $length)
+        );
+        return ($shortCode);
+    }
+
+
+    public function generateAlphaNumeric($length = 8, $isCapital = false) {
+
+        $listAlpha = 'abcdefghijklmnopqrstuvwxyz0123456789';        
+        $shortCode = str_shuffle(
+                substr(str_shuffle($listAlpha), 0, $length)
+        );
+        if($isCapital){
+            return strtoupper($shortCode);
+        }
+        return ($shortCode);
+    }   
 }
--- test/index.php
+++ test/index.php
@@ -1,18 +1,11 @@
-# randomcodegenerate
-
-composer require satish/randomcodegenerate
-
-
-How to use the random code generate
-
 <?php
-require_once './vendor/autoload.php';
-
+require_once '../src/RandomCodes.php';
 $objRandomCodes = new RandomCodes\RandomCodes();
 echo $objRandomCodes->generateCode();// 12 digit random number generate
-
+echo "<br />";
 echo $objRandomCodes->generateAlphabets($length = 8, $isCapital = true);
-
+echo "<br />";
 echo $objRandomCodes->generateNumericCode($length = 8, $incZero = true);
-
+echo "<br />";
 echo $objRandomCodes->generateAlphaNumeric($length = 8, $isCapital = true);
+echo "<br />";
